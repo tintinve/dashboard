03afe07686f7ee5e9b172622ae48b96720abf729
tintinve <pedroluismejia@gmail.com>
2018-09-27 22:17:15 +0200

    V0.01

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 import { format, formatDistance, formatRelative, subDays } from "date-fns";
+import { TimelineMax, CSSPlugin, ScrollToPlugin, Draggable } from "gsap/all";
 
 console.log(format(new Date(), "[Today is a] dddd"));
 //=> "Today is a Thursday"
-console.log(formatDistance(subDays(new Date(), 3), new Date()));
+// ---------------------------->
+function init() {
+  setInterval(update, 1000);
+}
+init();
+function update() {
+  let data = JSON.parse(FooBar.getData());
+  // OJO FooBar no es cammelCase
+  console.log(data);
+  handleBartenders(data.bartenders);
+}
+function handleBartenders(bartenders) {
+  console.log(bartenders);
+}
+// ---------------------------->
