0d8b21e39273e24cb479b6351d6f88f5d1b8c64b
tintinve <pedroluismejia@gmail.com>
2018-09-27 22:37:32 +0200

    Chart.js NPM module installed, integrated and oparative

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 import { format, formatDistance, formatRelative, subDays } from "date-fns";
 import { TimelineMax, CSSPlugin, ScrollToPlugin, Draggable } from "gsap/all";
+import Chart from "chart.js";
+var ctx = document.getElementById("myChart");
 
 console.log(format(new Date(), "[Today is a] dddd"));
 //=> "Today is a Thursday"
@@ -18,3 +20,43 @@ function handleBartenders(bartenders) {
   console.log(bartenders);
 }
 // ---------------------------->
+var myChart = new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)"
+        ],
+        borderColor: [
+          "rgba(255,99,132,1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)"
+        ],
+        borderWidth: 1
+      }
+    ]
+  },
+  options: {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true
+          }
+        }
+      ]
+    }
+  }
+});
