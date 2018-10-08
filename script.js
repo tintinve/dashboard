653923efde9be450d4cba0787d838a2ce25c8449
tintinve <pedroluismejia@gmail.com>
2018-10-08 21:35:24 +0200

    NPM module removed. Chart JS from CDN

--- script.js
+++ script.js
@@ -1,62 +1,56 @@
-import { format, formatDistance, formatRelative, subDays } from "date-fns";
-import { TimelineMax, CSSPlugin, ScrollToPlugin, Draggable } from "gsap/all";
-import Chart from "chart.js";
-var ctx = document.getElementById("myChart");
-
-console.log(format(new Date(), "[Today is a] dddd"));
-//=> "Today is a Thursday"
-// ---------------------------->
+var barChart = document.getElementById("myChart").getContext("2d");
 function init() {
-  setInterval(update, 1000);
+  let data = JSON.parse(FooBar.getData());
+  console.log(data);
+  drawGraph(data);
+  update(data);
 }
 init();
-function update() {
-  let data = JSON.parse(FooBar.getData());
-  // OJO FooBar no es cammelCase
+function update(data) {
+  setInterval(function() {
+    drawGraph(data);
+  }, 10000);
+  //This creates a parameterless anonymous function which calls drawGraph() with arguments
+}
+function drawGraph(data) {
   console.log(data);
-  handleBartenders(data.bartenders);
-}
-function handleBartenders(bartenders) {
-  console.log(bartenders);
-}
-// ---------------------------->
-var myChart = new Chart(ctx, {
-  type: "bar",
-  data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-    datasets: [
-      {
-        label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)"
-        ],
-        borderColor: [
-          "rgba(255,99,132,1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)"
-        ],
-        borderWidth: 1
-      }
-    ]
-  },
-  options: {
-    scales: {
-      yAxes: [
+  var chart = new Chart(barChart, {
+    type: "bar",
+    data: {
+      labels: [
+        data.storage[0].name,
+        data.storage[1].name,
+        data.storage[2].name,
+        data.storage[3].name,
+        data.storage[4].name,
+        data.storage[5].name,
+        data.storage[6].name,
+        data.storage[7].name,
+        data.storage[8].name,
+        data.storage[9].name
+      ],
+      datasets: [
         {
-          ticks: {
-            beginAtZero: true
-          }
+          label: "Storage Amount",
+          backgroundColor: "rgb(255, 99, 132)",
+          borderColor: "rgb(255, 99, 132)",
+          data: [
+            data.storage[0].amount,
+            data.storage[1].amount,
+            data.storage[2].amount,
+            data.storage[3].amount,
+            data.storage[4].amount,
+            data.storage[5].amount,
+            data.storage[6].amount,
+            data.storage[7].amount,
+            data.storage[8].amount,
+            data.storage[9].amount
+          ]
         }
       ]
-    }
-  }
-});
+    },
+
+    // Configuration options go here
+    options: {}
+  });
+}
